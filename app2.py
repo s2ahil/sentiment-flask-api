da2db7b94b194eebc426b5b3b76ebcb2f9a79a66
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-04-20 21:25:56 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,68 +1,77 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import *
-import pickle
-import sys
-import json
-from nltk.stem import WordNetLemmatizer
-import re      
-import pandas as pd
 
-app=Flask(__name__)
+app = Flask(__name__)
 
+@app.route('/',methods=['GET'])
+def hello_world():
+    return 'chal lawde!'
+
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#   shortcode= request.form.to_dict()
+#   # Get a list of all the values
+#   values = list(shortcode.values())
+#   print(values)
+#   text=values
+#   json_data = json.dumps(text)
+#   return json_data
 @app.route('/',methods=['POST'])
 def predict():
- try:
   shortcode= request.form.to_dict()
   # Get a list of all the values
   values = list(shortcode.values())
   print(values)
   text=values
 
-  with open('vectoriser-ngram-(1,2).pickle', 'rb') as f:
+  with open('./vectoriser-ngram-(1,2).pickle', 'rb') as f:
     vectorizer = pickle.load(f)
 
   with open('Sentiment-LR.pickle', 'rb') as f:
       model = pickle.load(f)
-  
+
   # Defining dictionary containing all emojis with their meanings.
-  emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+  emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad',
           ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
-          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed',
           ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
           '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
-          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink',
           ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
 
 
-  
+
   def preprocess(textdata):
-    
+
           processedText = []
-          
+
           # Create Lemmatizer and Stemmer.
           wordLemm = WordNetLemmatizer()
-          
+
           # Defining regex patterns.
           urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
           userPattern       = '@[^\s]+'
           alphaPattern      = "[^a-zA-Z0-9]"
           sequencePattern   = r"(.)\1\1+"
           seqReplacePattern = r"\1\1"
-          
+
           for tweet in textdata:
               tweet = tweet.lower()
-              
+
               # Replace all URls with 'URL'
               tweet = re.sub(urlPattern,' URL',tweet)
               # Replace all emojis.
               for emoji in emojis.keys():
-                  tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+                  tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])
               # Replace @USERNAME to 'USER'.
-              tweet = re.sub(userPattern,' USER', tweet)        
+              tweet = re.sub(userPattern,' USER', tweet)
               # Replace all non alphabets.
               tweet = re.sub(alphaPattern, " ", tweet)
               # Replace 3 or more consecutive letters by 2 letter.
               tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
-      
+
               tweetwords = ''
               for word in tweet.split():
                   # Checking if the word is a stopword.
@@ -71,38 +80,30 @@ def predict():
                       # Lemmatizing the word.
                       word = wordLemm.lemmatize(word)
                       tweetwords += (word+' ')
-                  
+
               processedText.append(tweetwords)
-              
+
           return processedText
 
-      
+
   def predict1(vectoriser, model, text):
       # Predict the sentiment
       textdata = vectoriser.transform(preprocess(text))
       sentiment = model.predict(textdata)
-      
+
       # Make a list of text with sentiment.
       data = []
       for text, pred in zip(text, sentiment):
           data.append((text,pred))
-          
+
       # Convert the list into a Pandas DataFrame.
       df = pd.DataFrame(data, columns = ['text','sentiment'])
       df = df.replace([0,1], ["Negative","Positive"])
       return df
-  
- 
-  
+
   df = predict1(vectorizer, model, text)
-  
-  print(df['sentiment'])   
-  return df['sentiment'].to_json()
 
- except Exception as e:
-        print(str(e))
-        import traceback
-        print(traceback.format_exc())
-
-if __name__ =="__main__":
-    app.run()
+  print(df['sentiment'])
+#   return df['sentiment'].to_json()
+  json_data = json.dumps(df['sentiment'])
+  return json_data
